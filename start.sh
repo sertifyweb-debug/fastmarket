12356f17fdf4d58b979dabe443c92490a3cf3641
sertifyweb-debug <sertify.web@gmail.com>
2026-03-01 03:18:27 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # install deps if you have them (safe even if requirements.txt is empty)
-python3 -m pip install --no-cache-dir -r requirements.txt
+python -m pip install --no-cache-dir -r requirements.txt
 
 # run your entrypoint
 python3 fastloop_trader.py
